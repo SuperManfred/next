b2f73e5ee229d179026e3beae885d7aaedeb26da
SuperManfred <Manfred.Neustifter@gmail.com>
2021-03-29 16:36:53 +1000

    mock SSR data

--- pages/form.js
+++ pages/form.js
@@ -1,16 +1,29 @@
 function Form() {
-    const registerUser = event => {
-      event.preventDefault() // don't redirect the page
-      // where we'll add our form logic
-    }
-  
-    return (
-      <form onSubmit={registerUser}>
-        <label htmlFor="name">Name</label>
-        <input id="name" type="text" autoComplete="name" required />
-        <button type="submit">Register</button>
-      </form>
-    )
+  const registerUser = async event => {
+    event.preventDefault()
+
+    const res = await fetch('/api/register', {
+      body: JSON.stringify({
+        name: event.target.name.value
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: 'POST'
+    })
+
+    const result = await res.json()
+    // result.user => 'SuperManfred'
   }
+
+  return (
+    <form onSubmit={registerUser}>
+      <label htmlFor="name">Name: </label>
+      <input id="name" name="name" type="text" autoComplete="name" required />
+      <button type="submit">Register</button>
+    </form>
+  )
+}
+
   
   export default Form
