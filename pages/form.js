04e9237faead50b35fac15f70e00cbb7b8778577
SuperManfred <Manfred.Neustifter@gmail.com>
2021-03-29 14:37:52 +1000

    add form.js page with export function Form

---
+++ pages/form.js
@@ -0,0 +1,16 @@
+function Form() {
+    const registerUser = event => {
+      event.preventDefault() // don't redirect the page
+      // where we'll add our form logic
+    }
+  
+    return (
+      <form onSubmit={registerUser}>
+        <label htmlFor="name">Name</label>
+        <input id="name" type="text" autoComplete="name" required />
+        <button type="submit">Register</button>
+      </form>
+    )
+  }
+  
+  export default Form
